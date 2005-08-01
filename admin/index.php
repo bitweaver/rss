c29f722f4ef52aef2edb699a6c86cb9821ddc5d9
Max Kremmel <xing@synapse.plus.com>
2005-08-01 18:41:20 +0000

    merge recent changes with HEAD - R1 and HEAD are identical now

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_rss/admin/index.php,v 1.1 2005/06/19 05:03:07 bitweaver Exp $
+// $Header: /cvsroot/bitweaver/_bit_rss/admin/index.php,v 1.2 2005/08/01 18:41:20 squareing Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -16,7 +16,7 @@ if (!isset($rsslib)) {
 }
 
 if (!$gBitUser->isAdmin()) {
-	$smarty->assign('msg', tra("You dont have permission to use this feature"));
+	$gBitSmarty->assign('msg', tra("You dont have permission to use this feature"));
 
 	$gBitSystem->display( 'error.tpl' );
 	die;
@@ -26,17 +26,17 @@ if (!isset($_REQUEST["rss_id"])) {
 	$_REQUEST["rss_id"] = 0;
 }
 
-$smarty->assign('rss_id', $_REQUEST["rss_id"]);
+$gBitSmarty->assign('rss_id', $_REQUEST["rss_id"]);
 
-$smarty->assign('preview', 'n');
+$gBitSmarty->assign('preview', 'n');
 
 if (isset($_REQUEST["view"])) {
-	$smarty->assign('preview', 'y');
+	$gBitSmarty->assign('preview', 'y');
 
 	$data = $rsslib->get_rss_module_content($_REQUEST["view"]);
 	$items = $rsslib->parse_rss_data($data, $_REQUEST["rss_id"]);
 
-	$smarty->assign_by_ref('items', $items);
+	$gBitSmarty->assign_by_ref('items', $items);
 }
 
 if ($_REQUEST["rss_id"]) {
@@ -53,12 +53,12 @@ if ($_REQUEST["rss_id"]) {
 	$info["show_pub_date"] = 'n';
 }
 
-$smarty->assign('name', $info["name"]);
-$smarty->assign('description', $info["description"]);
-$smarty->assign('url', $info["url"]);
-$smarty->assign('refresh', $info["refresh"]);
-$smarty->assign('show_title', $info["show_title"]);
-$smarty->assign('show_pub_date', $info["show_pub_date"]);
+$gBitSmarty->assign('name', $info["name"]);
+$gBitSmarty->assign('description', $info["description"]);
+$gBitSmarty->assign('url', $info["url"]);
+$gBitSmarty->assign('refresh', $info["refresh"]);
+$gBitSmarty->assign('show_title', $info["show_title"]);
+$gBitSmarty->assign('show_pub_date', $info["show_pub_date"]);
 
 if (isset($_REQUEST["remove"])) {
 	
@@ -69,33 +69,33 @@ if (isset($_REQUEST["save"])) {
 	
 
 	if (isset($_REQUEST['show_title']) == 'on') {
-		$smarty->assign('show_title', 'y');
+		$gBitSmarty->assign('show_title', 'y');
 		$info["show_title"] = 'y';
 	}
 	else
 	{
-		$smarty->assign('show_title', 'n');
+		$gBitSmarty->assign('show_title', 'n');
 		$info["show_title"] = 'n';
 	}
 	if (isset($_REQUEST['show_pub_date']) == 'on') {
-		$smarty->assign('show_pub_date', 'y');
+		$gBitSmarty->assign('show_pub_date', 'y');
 		$info["show_pub_date"] = 'y';
 	}
 	else
 	{
-		$smarty->assign('show_pub_date', 'n');
+		$gBitSmarty->assign('show_pub_date', 'n');
 		$info["show_pub_date"] = 'n';
 	}
 
 	$rsslib->replace_rss_module($_REQUEST["rss_id"], $_REQUEST["name"], $_REQUEST["description"], $_REQUEST["url"], $_REQUEST["refresh"], $info["show_title"], $info["show_pub_date"]);
 
-	$smarty->assign('rss_id', 0);
-	$smarty->assign('name', '');
-	$smarty->assign('description', '');
-	$smarty->assign('url', '');
-	$smarty->assign('refresh', 900);
-	$smarty->assign('show_title', 'n');
-	$smarty->assign('show_pub_date', 'n');
+	$gBitSmarty->assign('rss_id', 0);
+	$gBitSmarty->assign('name', '');
+	$gBitSmarty->assign('description', '');
+	$gBitSmarty->assign('url', '');
+	$gBitSmarty->assign('refresh', 900);
+	$gBitSmarty->assign('show_title', 'n');
+	$gBitSmarty->assign('show_pub_date', 'n');
 }
 
 if ( empty( $_REQUEST["sort_mode"] ) ) {
@@ -113,7 +113,7 @@ if (isset($_REQUEST['page'])) {
 	$page = &$_REQUEST['page'];
 	$offset = ($page - 1) * $maxRecords;
 }
-$smarty->assign_by_ref('offset', $offset);
+$gBitSmarty->assign_by_ref('offset', $offset);
 
 if (isset($_REQUEST["find"])) {
 	$find = $_REQUEST["find"];
@@ -121,29 +121,29 @@ if (isset($_REQUEST["find"])) {
 	$find = '';
 }
 
-$smarty->assign('find', $find);
+$gBitSmarty->assign('find', $find);
 
-$smarty->assign_by_ref('sort_mode', $sort_mode);
+$gBitSmarty->assign_by_ref('sort_mode', $sort_mode);
 $channels = $rsslib->list_rss_modules($offset, $maxRecords, $sort_mode, $find);
 
 $cant_pages = ceil($channels["cant"] / $maxRecords);
-$smarty->assign_by_ref('cant_pages', $cant_pages);
-$smarty->assign('actual_page', 1 + ($offset / $maxRecords));
+$gBitSmarty->assign_by_ref('cant_pages', $cant_pages);
+$gBitSmarty->assign('actual_page', 1 + ($offset / $maxRecords));
 
 if ($channels["cant"] > ($offset + $maxRecords)) {
-	$smarty->assign('next_offset', $offset + $maxRecords);
+	$gBitSmarty->assign('next_offset', $offset + $maxRecords);
 } else {
-	$smarty->assign('next_offset', -1);
+	$gBitSmarty->assign('next_offset', -1);
 }
 
 // If offset is > 0 then prev_offset
 if ($offset > 0) {
-	$smarty->assign('prev_offset', $offset - $maxRecords);
+	$gBitSmarty->assign('prev_offset', $offset - $maxRecords);
 } else {
-	$smarty->assign('prev_offset', -1);
+	$gBitSmarty->assign('prev_offset', -1);
 }
 
-$smarty->assign_by_ref('channels', $channels["data"]);
+$gBitSmarty->assign_by_ref('channels', $channels["data"]);
 
 
 // Display the template
