00598ecd73ff3f2ece0a2a9f41431fcc986ecf24
Max Kremmel <xing@synapse.plus.com>
2006-01-20 11:10:10 +0000

    merge recent changes into HEAD

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,6 +3,6 @@
 	$gBitSystem->registerPackage( 'rss', dirname( __FILE__).'/' );
 
 	if( $gBitSystem->isPackageActive( 'rss' ) ) {
-		$gBitSystem->registerAppMenu( RSS_PKG_DIR, 'RSS', RSS_PKG_URL.'index.php', 'bitpackage:rss/menu_rss.tpl', 'rss' );
+		$gBitSystem->registerAppMenu( RSS_PKG_NAME, 'RSS', RSS_PKG_URL.'index.php', 'bitpackage:rss/menu_rss.tpl', 'rss' );
 	}
 ?>
