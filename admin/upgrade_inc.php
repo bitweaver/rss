6e6e58a736429c758732c798b08515d3249864b9
Max Kremmel <xing@synapse.plus.com>
2006-02-01 12:09:00 +0000

    add de-tikification process to upgrade - untested

--- admin/upgrade_inc.php
+++ admin/upgrade_inc.php
@@ -8,18 +8,32 @@ $upgrades = array(
 	'BWR1' => array(
 // STEP 1
 array( 'DATADICT' => array( 
-array( 'RENAMECOLUMN' => array( 
-	'tiki_rss_modules' => array(	'`rssId`' => '`rss_id` I4 AUTO',
-								'`lastUpdated`' => '`last_updated` I8',
-								'`showPubDate`' => '`show_pub_date` I8',
-								'`showTitle`' => '`show_title` C(1)' ),
-	'tiki_rss_feeds' => array(	'`rssVer`' => '`rss_ver` C(1)',
-							'`lastUpdated`' => '`last_updated` I8'),
+	array( 'RENAMECOLUMN' => array( 
+		'tiki_rss_modules' => array(
+			'`rssId`' => '`rss_id` I4 AUTO',
+			'`lastUpdated`' => '`last_updated` I8',
+			'`showPubDate`' => '`show_pub_date` I8',
+			'`showTitle`' => '`show_title` C(1)' ),
+		'tiki_rss_feeds' => array(
+			'`rssVer`' => '`rss_ver` C(1)',
+			'`lastUpdated`' => '`last_updated` I8'),
+	)),
 )),
-))
 
 	)
-)
+),
+
+	'BWR1' => array(
+		'BWR2' => array(
+// de-tikify tables
+array( 'DATADICT' => array(
+	array( 'RENAMETABLE' => array(
+		'tiki_rss_modules' => 'rss_modules',
+		'tiki_rss_feeds' => 'rss_feeds',
+	)),
+)),
+		)
+	),
 
 );
 
