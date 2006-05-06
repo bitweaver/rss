a337309c4b6b79219d2b88e3ad5cbcbda9e04b7b
Max Kremmel <xing@synapse.plus.com>
2006-05-06 15:48:10 +0000

    clean up schema files, remove unused PackageInfo data - we really need a package manager for this, add plugin_path column to liberty_plugins

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -30,9 +30,6 @@ foreach( array_keys( $tables ) AS $tableName ) {
 $gBitInstaller->registerPackageInfo( RSS_PKG_NAME, array(
 	'description' => "Resource Description Framework (RDF) Site Summary (RSS) is a lightweight multipurpose extensible metadata description and syndication format. It allows users to read healines from your site with a dedicated RSS reader.",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'version' => '0.1',
-	'state' => 'alpha',
-	'dependencies' => '',
 ) );
 
 // ### Default Preferences
