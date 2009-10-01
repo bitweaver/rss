13f6da5b8c292f3bc77a75ff68a7559631527b27
wjames5 <will@tekimaki.com>
2009-10-01 14:17:03 +0000

    remove ref to non-existant copyright.txt file

--- rss_lib.php
+++ rss_lib.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_rss/rss_lib.php,v 1.18 2009/10/01 13:45:47 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_rss/rss_lib.php,v 1.19 2009/10/01 14:17:03 wjames5 Exp $
  * @package rss
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
  * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  *
- * $Id: rss_lib.php,v 1.18 2009/10/01 13:45:47 wjames5 Exp $
+ * $Id: rss_lib.php,v 1.19 2009/10/01 14:17:03 wjames5 Exp $
  */
 
 /**
